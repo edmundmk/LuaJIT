c9d292146b7ecafb54c3d3471dc2b21ed12b73db
Edmund Kapusniak <edmund.kapusniak@hotmail.com>
2020-09-22 13:32:16 +0100

    Change the way ipairs() works so that it returns the actual index.
    This means ipairs() returns -1 before the first call to its aux function.

--- src/lj_ffrecord.c
+++ src/lj_ffrecord.c
@@ -430,13 +430,8 @@ static void LJ_FASTCALL recff_ipairs_aux(jit_State *J, RecordFFData *rd)
     settabV(J->L, &ix.tabv, tabV(&rd->argv[0]));
     ix.val = 0; ix.idxchain = 0;
     ix.key = lj_opt_narrow_toint(J, J->base[1]);
-#if LJ_ZERO_BASED
-    J->base[1] = lj_record_idx(J, &ix);
-    J->base[0] = ix.key = emitir(IRTI(IR_ADD), ix.key, lj_ir_kint(J, 1));
-#else
     J->base[0] = ix.key = emitir(IRTI(IR_ADD), ix.key, lj_ir_kint(J, 1));
     J->base[1] = lj_record_idx(J, &ix);
-#endif
     rd->nres = tref_isnil(J->base[1]) ? 0 : 2;
   }  /* else: Interpreter will throw. */
 }
@@ -449,7 +444,7 @@ static void LJ_FASTCALL recff_xpairs(jit_State *J, RecordFFData *rd)
     if (tref_istab(tr)) {
       J->base[0] = lj_ir_kfunc(J, funcV(&J->fn->c.upvalue[0]));
       J->base[1] = tr;
-      J->base[2] = rd->data ? lj_ir_kint(J, 0) : TREF_NIL;
+      J->base[2] = rd->data ? lj_ir_kint(J, LJ_ZERO_BASED ? -1 : 0) : TREF_NIL;
       rd->nres = 3;
     }  /* else: Interpreter will throw. */
   }
