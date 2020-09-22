b972272d572421d86fb63e28900baec64e4c930b
Edmund Kapusniak <edmund.kapusniak@hotmail.com>
2020-09-22 10:41:41 +0100

    Change make.py not to use rsync, and change install name on macOS.

--- make.py
+++ make.py
@@ -1,11 +1,38 @@
 #!/usr/bin/env python3
 
 import os
-import sys
+import shutil
 import subprocess
+import sys
 
 build = sys.argv[ 1 ]
 output = sys.argv[ 2 ]
 
+# Make libluajit.
 subprocess.check_call( [ 'make', '-C', build ], env={ 'MACOSX_DEPLOYMENT_TARGET' : '10.9' } )
-subprocess.check_call( [ 'rsync', '-u', os.path.join( build, 'src', 'libluajit.so' ), output ] )
+input = os.path.join( build, 'src', 'libluajit.so' )
+
+# Check if the build product is newer than our output file.
+update = True
+if os.path.exists( output ):
+    intime = os.stat( input ).st_mtime
+    outtime = os.stat( output ).st_mtime
+    update = intime > outtime
+
+# Update output file.
+if update:
+
+    # Copy file over.
+    cp_file = output + ".install"
+    os.makedirs( os.path.dirname( output ), exist_ok = True )
+    shutil.copy2( input, cp_file )
+    try:
+        os.remove( output )
+    except FileNotFoundError:
+        pass
+    os.rename( cp_file, output )
+
+    # Update install name on macOS.
+    if shutil.which( "install_name_tool" ):
+        command = [ "install_name_tool", "-id", "@rpath/libluajit.dylib", output ]
+        subprocess.check_call( command )
